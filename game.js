dc35dc7feea99b705e19d60bf2e8867edf2cb0f3
xelphite <stoppanij@gmail.com>
2022-08-28 11:39:20 -0700

    base setup

---
+++ game.js
@@ -0,0 +1,10 @@
+// This is the main game variable. Stores a majority of one's individual game information and settings//
+
+const game = {
+    current: {
+
+    },
+    best: {
+        
+    }
+}
