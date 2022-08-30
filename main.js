392f5377b6f7b00f09627b80a3f4a7c36aafa03e
xelphite <stoppanij@gmail.com>
2022-08-30 15:20:00 -0700

    set up idle resource increasing, also added a function for removing resources generally for spending and other things

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+//General Purpose Start//
+
+const resources = ['herb', 'mythril', 'yew', 'crystal', 'arcana'];
+
+//General Purpose End//
+
 //Resource bar click functions start//
 
 const colorReset = () => {
@@ -40,22 +46,59 @@ const resourceBarClick = (resource) => {
 //Resource idle production update functions start//
 
 const updateTotalProduction = () => {
-	const resources = ['herb', 'mythril', 'yew', 'crystal', 'arcana'];
 	resources.map((resource) => {
 		const updating = game.current.resources[resource];
 		if (resource === game.current.collecting) {
-			updating.totalpersec = updating.golempersec + updating.activepersec;
+			updating.totalPerSec = updating.golemPerSec + updating.activePerSec;
 		} else {
-			updating.totalpersec = updating.golempersec;
+			updating.totalPerSec = updating.golemPerSec;
 		}
-		document.querySelector(`#${resource}-sec`).innerText = updating.totalpersec;
+		document.querySelector(`#${resource}-sec`).innerText = updating.totalPerSec;
 	});
 };
 
 //Resource idle production update functions end//
 
+//Resource update value functions start//
+
+const updateResourceAmountGain = (resource, value) => {
+	const updating = game.current.resources[resource];
+	if (updating.current + value > updating.storageMax) {
+		updating.current += updating.storageMax - updating.current;
+		updating.total += updating.storageMax - updating.current;
+	} else {
+		updating.current += value;
+		updating.total += value;
+	}
+	document.querySelector(`#${resource}-current`).innerText = updating.current;
+};
+
+const updateResourceAmountLoss = (resource, value) => {
+	const updating = game.current.resources[resource];
+	if (updating.current - value < 0) {
+		updating.current = 0;
+	} else {
+		updating.current += value;
+		updating.total += value;
+	}
+	document.querySelector(`#${resource}-current`).innerText = updating.current;
+};
+
+//Resource update value functions end//
+
 //Idle resource collection functions start//
 
-window.setInterval(() => {});
+const collectResources = () => {
+	resources.map((resource) => {
+		const updateValue = game.current.resources[resource].totalPerSec;
+		if (updateValue > 0) {
+			updateResourceAmountGain(resource, updateValue);
+		}
+	});
+};
+
+window.setInterval(() => {
+    collectResources()
+}, 100);
 
 //Idle resource collection functions end//
