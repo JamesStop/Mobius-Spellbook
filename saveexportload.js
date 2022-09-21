ebf284ffb740c3022e4daa5e20582d39dfb0c73a
xelphite <stoppanij@gmail.com>
2022-09-21 14:47:15 -0700

    created an export function that copies save string to clipboard, and and import function that updates the localstorage string to clipboard string and then reloads the game

--- saveexportload.js
+++ saveexportload.js
@@ -1,32 +1,44 @@
 // import LZString from "lz-string";
 
 const save = () => {
-    var saveString = JSON.stringify(game);
+	var saveString = JSON.stringify(game);
 	var saveGame = JSON.parse(saveString);
-    saveString = LZString.compressToBase64(JSON.stringify(saveGame));
-    localStorage.setItem('savestring', saveString);
-}
+	saveString = LZString.compressToBase64(JSON.stringify(saveGame));
+	localStorage.setItem('savestring', saveString);
+};
 
 const autoSave = () => {
-    save();
-    setTimeout(autoSave, 60000)
-}
+	save();
+	setTimeout(autoSave, 60000);
+};
 
 const load = () => {
-    if (localStorage.getItem('savestring')) {
-        savestring = localStorage.getItem('savestring');
-        savegame = JSON.parse(LZString.decompressFromBase64(savestring))
-        checkGameVersion(savegame);
-    } else {
-        game = currentGameVersion;
-    }
-        resourceColorReset();
-		setResourcesActiveColor(game.current.collecting);
-		updateResourceAmount();
-		updateTotalProductionAll();
-		updateStorages();
-		updateUpgradesDisplayAll();
-		golemAssignColors(game.current.resources.golems.assignmentType);
-		updateGolemsTotal();
-		updateGolemsActiveAll();
-}
+	if (localStorage.getItem('savestring')) {
+		savestring = localStorage.getItem('savestring');
+		savegame = JSON.parse(LZString.decompressFromBase64(savestring));
+		checkGameVersion(savegame);
+	} else {
+		game = currentGameVersion;
+	}
+	resourceColorReset();
+	setResourcesActiveColor(game.current.collecting);
+	updateResourceAmount();
+	updateTotalProductionAll();
+	updateStorages();
+	updateUpgradesDisplayAll();
+	golemAssignColors(game.current.resources.golems.assignmentType);
+	updateGolemsTotal();
+	updateGolemsActiveAll();
+};
+
+const exporting = () => {
+	if (localStorage.getItem('savestring')) {
+		navigator.clipboard.writeText(localStorage.getItem('savestring'));
+	}
+};
+
+const importing = async () => {
+	savestring = await navigator.clipboard.readText();
+	localStorage.setItem('savestring', savestring);
+	load();
+};
